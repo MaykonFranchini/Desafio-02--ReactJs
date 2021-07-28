b5035d905cfc7844d79e26f74b4fda2f50c1af41
Maykon Franchini <maykonfranchini2017@gmail.com>
2021-07-29 00:46:38 +0100

    header functionallity done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,7 +22,7 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
   // const { addProduct, cart } = useCart();
 
   // const cartItemsAmount = cart.reduce((sumAmount, product) => {
@@ -31,7 +31,7 @@ const Home = (): JSX.Element => {
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      api.get('products').then(response => setProducts(response.data));
     }
 
     loadProducts();
@@ -43,10 +43,12 @@ const Home = (): JSX.Element => {
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
+      {products.map(product => {
+        return (
+          <li key={product.id}>
+        <img src={product.image} alt={product.title} />
+        <strong>{product.title}</strong>
+        <span>{product.price}</span>
         <button
           type="button"
           data-testid="add-product-button"
@@ -54,12 +56,14 @@ const Home = (): JSX.Element => {
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
+            {/* {cartItemsAmount[product.id] || 0} */}
           </div>
 
           <span>ADICIONAR AO CARRINHO</span>
         </button>
       </li>
+        )
+      })}
     </ProductList>
   );
 };
